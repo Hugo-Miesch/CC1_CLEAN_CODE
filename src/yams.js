d2fedfe7f75741fa886f40c148853211f475a0b3
GOGO <hugo.miesch@epitech.eu>
2026-01-29 14:45:25 +0100

    [ADD] fonction qui regarde si 5 meme dés sont présent dans les lancés

--- src/yams.js
+++ src/yams.js
@@ -11,6 +11,32 @@ function checkBrelan(dice) {
     return 0;
 }
 
+function checkSquare(dice) {
+    for (let roll of dice) {
+        const counts = {};
+        for (let die of roll) {
+            counts[die] = (counts[die] || 0) + 1;
+            if (counts[die] >= 4) {
+                return 35;
+            }
+        }
+    }
+    return 0;
+}
+
+function checkYams(dice) {
+    for (let roll of dice) {
+        const counts = {};
+        for (let die of roll) {
+            counts[die] = (counts[die] || 0) + 1;
+            if (counts[die] >= 5) {
+                return 50;
+            }
+        }
+    }
+    return 0;
+}
+
 function getHighestSum(dice) {
     let maxSum = 0;
     for (let roll of dice) {
@@ -24,6 +50,15 @@ function getHighestSum(dice) {
 
 export function scoreYams(dice) {
     const brelanScore = checkBrelan(dice);
+    const squareScore = checkSquare(dice);
+    const yamsScore = checkYams(dice);
+
+    if (yamsScore > 0) {
+        return yamsScore;
+    }
+    if (squareScore > 0) {
+        return squareScore;
+    }
     if (brelanScore > 0) {
         return brelanScore;
     }
--- src/yams.test.js
+++ src/yams.test.js
@@ -8,5 +8,11 @@ describe('Return the highest Yams score from the given dice', () => {
     it('returns 28 if any roll contains at least three of a kind', () => {
         expect(scoreYams([[1, 2, 3, 4, 6], [2, 2, 2, 4, 6]])).toBe(28);
     })
+    it('returns 35 if any roll contains at least four of a kind', () => {
+        expect(scoreYams([[1, 2, 3, 4, 6], [5, 5, 5, 5, 2]])).toBe(35);
+    })
+    it('returns 50 if any roll contains five of a kind', () => {
+        expect(scoreYams([[1, 2, 3, 4, 6], [4, 4, 4, 4, 4]])).toBe(50);
+    })
 })
     
